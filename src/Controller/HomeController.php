40d82a5c7a12cdf87031c674f899b453cde54669
Benjamin Owen <benjamin@projecttiy.com>
2024-10-09 13:40:26 -0400

    updating _navbar template partial with new styles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,53 @@
 <?php
 
+/**
+ * Symfony Controller for / Route
+ *
+ * PHP version 8.3
+ *
+ * @category  Controller
+ * @package   PDF2CSV
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://mit-license.org/ MIT
+ * @version   CVS: $Id:$
+ * @link      https://github.com/benowe1717/games-projecttiy-com
+ **/
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * Symfony Controller for / Route
+ *
+ * PHP version 8.3
+ *
+ * @category  Controller
+ * @package   PDF2CSV
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://mit-license.org/ MIT
+ * @version   Release: 0.0.1
+ * @link      https://github.com/benowe1717/games-projecttiy-com
+ **/
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    /**
+     * / app_home Route
+     *
+     * @return Response
+     **/
+    #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render(
+            'home/index.html.twig',
+            [
+                'controller_name' => 'HomeController',
+            ]
+        );
     }
 }
