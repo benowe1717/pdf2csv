b43946cc669796692e05147f26994cca13c43395
Benjamin Owen <benjamin@projecttiy.com>
2024-10-11 10:41:44 -0400

    removed theme_switcher from app.js entry point, added theme_switcher as first entry in <head> in base template

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,6 @@
  * which should already be in your base.html.twig.
  */
 import 'bootstrap';
-import './theme_switcher.js';
+// import './theme_switcher.js';
 
 // console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
