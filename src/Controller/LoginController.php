046bc2b7f27d38efdd522a9c730b71675948e633
Benjamin Owen <78978715+benowe1717@users.noreply.github.com>
2024-10-14 15:17:52 -0400

    create login page for users (#19)
    * updated security config with new form_login method targeting the app_login route, ensured that csrf protection is enabled for the form, updated the LoginController to add the app_logout route, updated the login twig template to use the new login form
    
    * updated navbar template to use a danger color for sign out button, updated home template to use the tertiary orange/yellow color instead of grey for a 2nd button, updated login template to have danger banner when sign in fails, added a forgot password button to the login template in prep for when this feature is available
    
    closes #7

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -16,9 +16,11 @@
 
 namespace App\Controller;
 
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * Symfony Controller for /login Route
@@ -38,16 +40,37 @@ class LoginController extends AbstractController
     /**
      * /app_login Route
      *
+     * @param AuthenticationUtils $authenticationUtils The authentication utils
+     *
      * @return Response
      **/
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // get the last username entered
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render(
             'login/index.html.twig',
             [
                 'controller_name' => 'LoginController',
+                'last_username' => $lastUsername,
+                'error' => $error
             ]
         );
+    }
+
+    /**
+     * /app_logout Route
+     *
+     * @return void
+     **/
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new LogicException('This does nothing, the firewall handles this');
     }
 }
