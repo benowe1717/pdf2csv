3bae9bc3a48b32a297a52162c713c332ad101361
Benjamin Owen <benjamin@projecttiy.com>
2024-10-15 10:16:50 -0400

    updated deployment_notes, created deploy.sh to help with consistent deployments

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PHP=`which php`
+COMPOSER=`which composer`
+
+BINARIES=("php" "composer")
+
+APP_ENV="prod"
+
+for ((i=0; i < ${#BINARIES[@]}; i++)); do
+    RESULT=$(which ${BINARIES[i]} > /dev/null 2>&1; echo $?)
+    if [[ "$RESULT" != 0 ]]; then
+        /bin/echo "ERROR: Unable to locate ${BINARIES[i]}!"
+        exit 1
+    fi
+done
+
+$COMPOSER dump-env $APP_ENV
+$COMPOSER install --no-dev --optimize-autoloader
+$PHP ./bin/console doctrine:migrations:migrate
+$PHP ./bin/console importmap:install
+$PHP ./bin/console sass:build
+$PHP ./bin/console asset-map:compile
+$PHP ./bin/console cache:clear
