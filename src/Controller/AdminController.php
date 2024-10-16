ca5958d4f34819bddf29f3c71be2ab3de68f2843
Benjamin Owen <benjamin@projecttiy.com>
2024-10-15 22:01:30 -0400

    ran `php bin/console make:controller` to get the app_admin route created

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,54 @@
+<?php
+
+
+/**
+ * Symfony Controller for /admin Route
+ *
+ * PHP version 8.3
+ *
+ * @category  Controller
+ * @package   PDF2CSV
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   CVS: $Id:$
+ * @link      https://github.com/benowe1717/pdf2csv
+ **/
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Symfony Controller for /admin Route
+ *
+ * PHP version 8.3
+ *
+ * @category  Controller
+ * @package   PDF2CSV
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   Release: 0.0.1
+ * @link      https://github.com/benowe1717/pdf2csv
+ **/
+class AdminController extends AbstractController
+{
+    /**
+     * /app_admin Route
+     *
+     * @return Response
+     **/
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render(
+            'admin/index.html.twig',
+            [
+                'controller_name' => 'AdminController',
+            ]
+        );
+    }
+}
