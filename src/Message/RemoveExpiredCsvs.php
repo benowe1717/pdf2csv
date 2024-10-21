dd5054ce80be87fb47b52a3f36de21ae35393f75
Benjamin Owen <benjamin@projecttiy.com>
2024-10-20 21:12:38 -0400

    ran `composer require dragonmantank/cron-expression` to bring in cron-like syntax for scheduling, ran `php bin/console make:message` to create the ExpireCsvs Message class and Handler and the RemoveExpiredCsvs Message class and Handler, ran `php bin/console make:schedule` to create the main schedule for handling both messages

---
+++ src/Message/RemoveExpiredCsvs.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Symfony Message for Removing Expired CSVs
+ *
+ * PHP version 8.3
+ *
+ * @category  Message
+ * @package   PDF2CSV
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   CVS: $Id:$
+ * @link      https://github.com/benowe1717/pdf2csv
+ **/
+
+namespace App\Message;
+
+/**
+ * Symfony Message for Removing Expired CSVs
+ *
+ * PHP version 8.3
+ *
+ * @category  Message
+ * @package   PDF2CSV
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   Release: 0.0.1
+ * @link      https://github.com/benowe1717/pdf2csv
+ **/
+final class RemoveExpiredCsvs
+{
+    /**
+     * Message Constructor
+     **/
+    public function __construct()
+    {
+    }
+
+    /**
+     * Getter for $expiredCSVs
+     *
+     * @return array
+     **/
+    public function removeExpiredCSVs(): void
+    {
+    }
+
+    /*
+     * Add whatever properties and methods you need
+     * to hold the data for this message class.
+     */
+
+    // public function __construct(
+    //     public readonly string $name,
+    // ) {
+    // }
+}
