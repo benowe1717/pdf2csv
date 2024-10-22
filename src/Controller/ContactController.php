a7c410473a67e1cec22c46d3f9ad0d80d78f929b
Benjamin Owen <78978715+benowe1717@users.noreply.github.com>
2024-10-22 10:27:15 -0400

    submit requests to admin (#30)
    * created new ContactType form, updated ContactController to create and render new form, updated contact twig template to render new form
    
    * updated ContactController to store form data in a variable that will be emitted to a message later, updated twig template to render new svg and updated link to send to new HelpDesk software instead of mastodon
    
    closes #13

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,7 +16,9 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -30,7 +32,7 @@ use Symfony\Component\Routing\Attribute\Route;
  * @author    Benjamin Owen <benjamin@projecttiy.com>
  * @copyright 2024 Benjamin Owen
  * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
- * @version   Release: 0.0.1
+ * @version   Release: 0.0.2
  * @link      https://github.com/benowe1717/pdf2csv
  **/
 class ContactController extends AbstractController
@@ -38,15 +40,31 @@ class ContactController extends AbstractController
     /**
      * /app_contact Route
      *
+     * @param Request $request The http request
+     *
      * @return Response
      **/
-    #[Route('/contact', name: 'app_contact', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+        $newMessage = array();
+
+        $newMessageForm = $this->createForm(ContactType::class);
+        $newMessageForm->handleRequest($request);
+
+        $errors = $newMessageForm->getErrors(true);
+        foreach ($errors as $error) {
+            $this->addFlash('newMessageFormErrors', $error->getMessage());
+        }
+
+        if ($newMessageForm->isSubmitted() && $newMessageForm->isValid()) {
+            $newMessage = $newMessageForm->getData();
+        }
+
         return $this->render(
             'contact/index.html.twig',
             [
-                'controller_name' => 'HomeController',
+                'new_message_form' => $newMessageForm,
             ]
         );
     }
