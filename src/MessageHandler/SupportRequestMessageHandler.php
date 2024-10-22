1aed642fa2408f09df52c692fc0ee88b3ccf8125
Benjamin Owen <benjamin@projecttiy.com>
2024-10-22 16:11:00 -0400

    updated ContactController to add flash messages on form submission and form success, updated ContactController to emit SupportRequestMessage message on successful form submissions, updated SupportRequestMessage to support accepting a message from the Contact form, updated SupportRequestMessageHandler to send an email using the submitted form values, updated contact twig template to display flash messages, updated contact twig template to change the helpdesk link, created the support_request email twig template

--- src/MessageHandler/SupportRequestMessageHandler.php
+++ src/MessageHandler/SupportRequestMessageHandler.php
@@ -1,15 +1,83 @@
 <?php
 
+/**
+ * Symfony Message Handler for SupportRequestMessage
+ *
+ * PHP version 8.3
+ *
+ * @category  MessageHandler
+ * @package   PDF2CSV
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   CVS: $Id:$
+ * @link      https://github.com/benowe1717/pdf2csv
+ **/
+
 namespace App\MessageHandler;
 
 use App\Message\SupportRequestMessage;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Address;
 
+/**
+ * Symfony Message Handler for SupportRequestMessage
+ *
+ * PHP version 8.3
+ *
+ * @category  MessageHandler
+ * @package   PDF2CSV
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GNU GPLv3
+ * @version   Release: 0.0.1
+ * @link      https://github.com/benowe1717/pdf2csv
+ **/
 #[AsMessageHandler]
 final class SupportRequestMessageHandler
 {
+    private MailerInterface $mailer;
+
+    /**
+     * SupportRequestMessageHandler constructor
+     *
+     * @param MailerInterface $mailer The mailer interface
+     **/
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * Method actions the data given in the message
+     *
+     * @param SupportRequestMessage $message The message to handle
+     *
+     * @return void
+     **/
     public function __invoke(SupportRequestMessage $message): void
     {
-        // do something with your message
+        $data = $message->getContent();
+
+        $sendEmail = (new TemplatedEmail())
+            ->from(
+                new Address(
+                    'pdf2csv@projecttiy.com',
+                    'PDF2CSV Support'
+                )
+            )
+            ->to('support@projecttiy.com')
+            ->subject('PDF2CSV Support Request')
+            ->htmlTemplate('contact/support_request.html.twig')
+            ->context(
+                [
+                    'full_name' => $data['name'],
+                    'email_address' => $data['email'],
+                    'message' => $data['message']
+                ]
+            );
+        $this->mailer->send($sendEmail);
     }
 }
